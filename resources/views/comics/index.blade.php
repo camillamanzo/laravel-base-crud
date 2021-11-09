c0a5e9aae4795ec3411d0c20f9e9f1650a95f024
camillamanzo <camillasouthafrica@gmail.com>
2021-11-09 11:23:27 +0100

    moved button in index for better view

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -2,23 +2,24 @@
 @extends('layouts.main')
 
 @section('content')
-    <h1 class="my-5 text-center">Best Comics:</h1>
+    <h1 class="mt-5 text-center">Best Comics:</h1>
     <div class="container">
         <div class="row justify-content-center">
-
+                        
+            <a class="mb-5 text-center nav-link @if (request()->routeIs('comics.create')) active @endif" href="{{ route('comics.create') }}">
+                <button class="btn btn-primary px-3">Add a comic</button>
+            </a>
+            
             @foreach ($comics as $comic )
                 <div class="col-3 text-center">
                     <img src="{{$comic->img_url}}" width="200" alt="">
                     <h3 class="my-3">{{$comic->title}}</h3>
                     <a href="{{ route('comics.show', ['comic'=> $comic->id])}}">Read more</a>
                     <br>
-                    <a href="{{ route('comics.edit', $comic->id)}}">Modify comic</a>
+                    <a href="{{ route('comics.edit', $comic->id) }}">Modify comic</a>
                 </div>
             @endforeach
-            
-            <a class="my-5 text-center nav-link @if (request()->routeIs('comics.create')) active @endif" href="{{ route('comics.create') }}">
-                <button class="btn btn-primary px-3">Add a comic</button>
-            </a>
+
         </div>
     </div>
 @endsection
